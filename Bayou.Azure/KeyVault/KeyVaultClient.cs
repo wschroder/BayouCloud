50adf0969fc025643262ee0666fe816b8bf23dbe
Wayne Schroder <GratefulBayou@gmail.com>
2021-03-28 16:16:15 -0400

    Move keyvault code over into Bayou.Azure

---
+++ Bayou.Azure/KeyVault/KeyVaultClient.cs
@@ -0,0 +1,37 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using System;
+using System.Collections.Generic;
+
+namespace Bayou.Azure.KeyVault
+{
+    public static class KeyVaultManager
+    {
+        public static List<KeyValuePair<string, string>> DownloadKeyVaultValues(string keyVaultName)
+        {
+            // Note: Access to the keyvault relies on Role Based Access Control (RBAC), 
+            // with this console application's identify configured as environment variables
+            // (AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, AZURE_TENANT_ID) that are picked up by
+            // DefaultAzureCredentials.
+            // Sett Setup-KeyVault.ps1 for details on setting this up.
+
+            var kvUri = $"https://{keyVaultName}.vault.azure.net";
+
+            var cred = new DefaultAzureCredential();
+            // var cred = new VisualStudioCredential();
+
+            var client = new SecretClient(new Uri(kvUri), cred);
+
+            List<KeyValuePair<string, string>> secretsList = new List<KeyValuePair<string, string>>();
+
+            IEnumerable<SecretProperties> secretProperties = client.GetPropertiesOfSecrets();
+            foreach (SecretProperties secret in secretProperties)
+            {
+                KeyVaultSecret secretWithValue = client.GetSecret(secret.Name);
+                var nameValuePair = new KeyValuePair<string, string>(secretWithValue.Name, secretWithValue.Value);
+                secretsList.Add(nameValuePair);
+            }
+            return secretsList;
+        }
+    }
+}
--- Bayou.Cloud.IntegrationTest/Program.cs
+++ Bayou.Cloud.IntegrationTest/Program.cs
@@ -1,5 +1,4 @@
-﻿using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
+﻿using Bayou.Azure.KeyVault;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
@@ -23,7 +22,6 @@ namespace Bayou.Cloud.IntegrationTest
             var queueTester = new StorageQueueTester(storageServiceConnectionString);
 
             await queueTester.RunTest();
-        
         }
 
         private static IConfigurationRoot MergeInKeyVaultConfig(IConfigurationRoot config, string keyVaultName)
@@ -32,38 +30,11 @@ namespace Bayou.Cloud.IntegrationTest
 
             configBuilder.AddConfiguration(config);
 
-            List<KeyValuePair<string, string>> secretsList = DownloadKeyVaultValues(keyVaultName);
+            List<KeyValuePair<string, string>> secretsList = KeyVaultManager.DownloadKeyVaultValues(keyVaultName);
 
             configBuilder.AddInMemoryCollection(secretsList);
 
             return configBuilder.Build();
-        }
-
-        private static List<KeyValuePair<string, string>> DownloadKeyVaultValues(string keyVaultName)
-        {
-            // Note: Access to the keyvault relies on Role Based Access Control (RBAC), 
-            // with this console application's identify configured as environment variables
-            // (AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, AZURE_TENANT_ID) that are picked up by
-            // DefaultAzureCredentials.
-            // Sett Setup-KeyVault.ps1 for details on setting this up.
-
-            var kvUri = $"https://{keyVaultName}.vault.azure.net";
-
-            var cred = new DefaultAzureCredential();
-            // var cred = new VisualStudioCredential();
-
-            var client = new SecretClient(new Uri(kvUri), cred);
-
-            List<KeyValuePair<string, string>> secretsList = new List<KeyValuePair<string, string>>();
-
-            IEnumerable<SecretProperties> secretProperties = client.GetPropertiesOfSecrets();
-            foreach (SecretProperties secret in secretProperties)
-            {
-                KeyVaultSecret secretWithValue = client.GetSecret(secret.Name);
-                var nameValuePair = new KeyValuePair<string, string>(secretWithValue.Name, secretWithValue.Value);
-                secretsList.Add(nameValuePair);
-            }
-            return secretsList;
         }
 
         private static IConfigurationRoot SetupConfiguration()
